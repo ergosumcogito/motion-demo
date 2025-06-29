d8f0cba59c290ec6eb178bc978a6c18acf178312
Your Name <you@example.com>
2025-06-29 19:31:48 +0200

    Fix Navbar alignment

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,9 +8,10 @@ export default function NavBar() {
             background: "#22004e",
             display: "flex",
             alignItems: "center",
+            fontSize: "2.0rem"
         }}>
-            <Link to="/" style={{ margin: "10px" }}>
-                <img src={homeIcon} alt="Home" width={35} />
+            <Link to="/">
+                <img src={homeIcon} alt="Home" width={35} style={{ verticalAlign: "middle", margin: "10px" }} />
             </Link>
             <Link to="/page-one" style={{color: "white", margin: "10px" }}>Page 1</Link>
             <Link to="/page-two" style={{color: "white", margin: "10px" }}>Page 2</Link>
