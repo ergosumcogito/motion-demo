d67f71183412dfd77c2e926e346ce9f7a543531d
Your Name <you@example.com>
2025-06-30 17:43:08 +0200

    Make horizontal transition animation

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -10,10 +10,10 @@ function PageWrapper({children, enabled = true}) {
     // With animations
     return (
         <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -20 }}
-            transition={{ duration: 0.4 }}
+            initial={{ opacity: 0, x: -20 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 40 }}
+            transition={{ duration: 0.27 }}
         >
             {children}
         </motion.div>
